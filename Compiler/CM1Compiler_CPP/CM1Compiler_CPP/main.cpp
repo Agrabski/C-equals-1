d33bf69888b51e89af38a362ab00fbdb35e79e35
Adam Grabski <adam.gr@outlook.com>
2021-10-30 00:07:36 +0200

    .

--- Compiler/CM1Compiler_CPP/CM1Compiler_CPP/main.cpp
+++ Compiler/CM1Compiler_CPP/CM1Compiler_CPP/main.cpp
@@ -13,12 +13,25 @@
 #include "../Compiler/ExceptionHandling.hpp"
 #include "../Compiler/LLVMIntegration/LLVMIRUtility.hpp"
 #include "../LanguageLogic/CompileTimeInfrastrucutre/CompilationOptions.hpp"
+#include "../LanguageLogic/CompilerInterface.hpp"
 
 using cMCompiler::language::compileTimeInfrastructure::CompilationOptions;
+class CompilerInterface : cMCompiler::language::CompilerInterface
+{
+public:
+	CompilerInterface() : cMCompiler::language::CompilerInterface() {}
+	std::vector< cMCompiler::dataStructures::validationError> errors;
+	virtual void raiseError(std::string const& text, cMCompiler::dataStructures::SourcePointer const& source, unsigned long long const code) override
+	{
+		errors.push_back({ text, source, code });
+		std::cout << "error " << code << ": " << text << " " << source.filePath_ << " line: " << source.lineNumber_ << std::endl;
+	}
+};
 
 int main(int argc, char* argv[])
 {
 	using namespace std::literals;
+	CompilerInterface interf{};
 
 	auto context = cMCompiler::getCompilationContext(argc, argv);
 	if (context)
@@ -47,11 +60,20 @@ int main(int argc, char* argv[])
 				{
 					auto llvmIr = cMCompiler::compiler::llvmIntegration::compileToLLVMIr(*compiler.front(), packages, context);
 
+					if (interf.errors.empty())
+					{
+						auto tripple = llvm::sys::getDefaultTargetTriple();
+						cMCompiler::compiler::llvmIntegration::compileToBinary(llvmIr.get(), "out", tripple);
 
-					auto tripple = llvm::sys::getDefaultTargetTriple();
-					cMCompiler::compiler::llvmIntegration::compileToBinary(llvmIr.get(), "out", tripple);
-
-					llvmIr.release();
+						llvmIr.release();
+					}
+					else
+					{
+						for(auto error : interf.errors)
+							std::cout << "error " << error.code << ": " << error.text << " " << error.source.filePath_ << " line: " << error.source.lineNumber_ << std::endl;
+						std::cout << interf.errors.size() << " errors occured. Compilation terminated"<<std::endl;
+						return -1;
+					}
 				});
 		}
 		catch (std::exception const& e)
